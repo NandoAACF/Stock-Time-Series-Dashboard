ab46ca9b40f16e839ec892af0be49e9b2ec0cd2c
NandoAACF <nando.aacf88@gmail.com>
2023-12-24 19:00:18 +0700

    fix: update info page

--- page_info.py
+++ page_info.py
@@ -5,8 +5,9 @@ def show_info():
     st.title('Data Information')
 
     st.image('images/yahoo_finance.png')
-    st.write('Data yang digunakan adalah data yang diambil dari Yahoo Finance mulai tanggal 1 September 2019 sampai sekarang.')
-    st.write('User dapat memasukkan kode ticker saham yang ingin dianalisis. Program akan memvisualisasikan analisis dari data saham tersebut.')
+    st.write('Data di-scraping dari Yahoo Finance mulai tanggal 1 September 2019 sampai sekarang.')
+    st.write('Feature-nya berupa Date, Open, High, Low, Close, Volume, dan Dividen.')
+    st.write('User dapat memasukkan kode ticker saham yang ingin dianalisis. Program akan memvisualisasikan analisis dari data saham sesuai keinginan user.')
 
     st.image('images/yfinance.png')
     st.write('Scraping Yahoo Finance menggunakan Python dengan library yfinance.')
